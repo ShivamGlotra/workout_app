c77e57dc72123a751a2d389d9b1dffc163dc9d2e
Shivam Glotra <shivam.glotra.43@gmail.com>
2026-01-26 21:48:37 -0500

    home page UI done

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -17,6 +17,18 @@ class _HomePageState extends State<HomePage> {
     {"title": "AI Coach", "icon": Icons.smart_toy},
     {"title": "Profile", "icon": Icons.person},
   ];
+
+  final List<Map<String, dynamic>> exerciseTiles = [
+    {"title": "CHEST", "image": "assets/images/chest.png"},
+    {"title": "BACK", "image": "assets/images/back.png"},
+    {"title": "SHOULDERS", "image": "assets/images/shoulder.png"},
+    {"title": "LEGS", "image": "assets/images/legs.png"},
+    {"title": "ARMS", "image": "assets/images/biceps.png"},
+    {"title": "CORE", "image": "assets/images/core.png"},
+    {"title": "CARDIO", "image": "assets/images/cardio.png"},
+    {"title": "30 Min WORKOUT", "image": "assets/images/triceps.png"},
+  ];
+
   int _selectedIndex = 0;
   void _onItemTap(int index) {
     setState(() {
@@ -29,13 +41,56 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Let's Workout"), centerTitle: true),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: SizedBox(
+                height: 55,
+                width: double.infinity,
+                child: SearchBar(
+                  hintText: 'Search exercises',
+                  leading: const Icon(Icons.search),
+                  elevation: MaterialStateProperty.all(1),
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 25),
+            ...List.generate((exerciseTiles.length / 2).ceil(), (rowIndex) {
+              int firstIndex = rowIndex * 2;
+              int secondIndex = firstIndex + 1;
 
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    exerciseTile(exerciseTiles[firstIndex]),
+                    if (secondIndex < exerciseTiles.length)
+                      const SizedBox(width: 20),
+                    if (secondIndex < exerciseTiles.length)
+                      exerciseTile(exerciseTiles[secondIndex]),
+                  ],
+                ),
+              );
+            }),
+          ],
+        ),
+      ),
       drawer: Drawer(
         child: ListView(
           children: [
             SizedBox(
               height: 80,
-
               child: Center(
                 child: Text(
                   "Let's Workout",
@@ -97,6 +152,41 @@ class _HomePageState extends State<HomePage> {
             ),
             onTap: onTap,
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget exerciseTile(Map<String, dynamic> data) {
+    return SizedBox(
+      height: 140,
+      width: 180,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset(data['image'], fit: BoxFit.cover),
+            Material(
+              color: Colors.black26, // semi-transparent overlay
+              child: InkWell(
+                onTap: () {
+                  // handle button tap
+                },
+                child: Center(
+                  child: Text(
+                    data['title'],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 204, 243, 247),
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
