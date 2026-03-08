4b08b20ab0e8743e069863bbc2c8d5324b855860
Shivam <shivam.glotra.43@gmail.com>
2026-03-08 01:34:49 -0500

    calorie calc has widgets added

--- lib/screens/calorie_calculator.dart
+++ lib/screens/calorie_calculator.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class CalorieCalculatorScreen extends StatefulWidget {
@@ -11,6 +12,11 @@ class _BmiCalculatorScreenState extends State<CalorieCalculatorScreen> {
   int? _selectedGender;
   String selectedActivityValue = 'Sedentary (little or no exercise)';
   String selectedGoalValue = 'Maintain weight';
+  int bmrRate = 0;
+  int maintainanceCalorie = 0;
+  int targetCalories = 0;
+  bool userClickedCalculate = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,35 +71,18 @@ class _BmiCalculatorScreenState extends State<CalorieCalculatorScreen> {
                         ),
                       ),
                       SizedBox(height: 10),
-                      Container(
-                        padding: EdgeInsets.only(
-                          top: 20,
-                          bottom: 20,
-                          left: 40,
-                          right: 40,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(
-                            color: const Color.fromARGB(
-                              255,
-                              221,
-                              219,
-                              219,
-                            ), // border color
-                            width: 1, // border width
-                          ),
-                          borderRadius: BorderRadius.circular(
-                            12,
-                          ), // rounded corners
-                        ),
-
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [SizedBox(height: 10), calculator()],
-                        ),
+                      widgetContainer(customWidget: calculator()),
+                      SizedBox(height: 20),
+                      widgetContainer(customWidget: calorieResultsWidget()),
+                      SizedBox(height: 20),
+                      widgetContainer(
+                        customWidget: understandingResultWidget(),
                       ),
-                      SizedBox(height: 50),
+                      SizedBox(height: 20),
+                      widgetContainer(customWidget: macronutritionWidget()),
+                      SizedBox(
+                        height: MediaQuery.of(context).padding.bottom + 30,
+                      ),
                     ],
                   ),
                 ),
@@ -101,6 +90,24 @@ class _BmiCalculatorScreenState extends State<CalorieCalculatorScreen> {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget widgetContainer({required Widget customWidget}) {
+    return Container(
+      padding: EdgeInsets.only(top: 20, bottom: 30, left: 30, right: 30),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: const Color.fromARGB(255, 221, 219, 219), // border color
+          width: 1, // border width
+        ),
+        borderRadius: BorderRadius.circular(12), // rounded corners
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [customWidget],
       ),
     );
   }
@@ -282,7 +289,11 @@ class _BmiCalculatorScreenState extends State<CalorieCalculatorScreen> {
         ),
         SizedBox(height: 30),
         TextButton(
-          onPressed: () => (),
+          onPressed: () => (setState(() {
+            bmrRate = 1500 + Random().nextInt(2000 - 1500 + 1);
+            maintainanceCalorie = 3200 + Random().nextInt(3500 - 3200 + 1);
+            targetCalories = 2600 + Random().nextInt(3000 - 2600 + 1);
+          })),
           style: TextButton.styleFrom(
             backgroundColor: Colors.black,
             foregroundColor: Colors.white,
@@ -292,6 +303,280 @@ class _BmiCalculatorScreenState extends State<CalorieCalculatorScreen> {
             ),
           ),
           child: Text("Calculate Calories"),
+        ),
+      ],
+    );
+  }
+
+  Widget calorieResultsWidget() {
+    final List<Map<String, dynamic>> resultCards = [
+      {
+        "icon": Icons.trending_up_outlined,
+        "title": "Base Metabolic Rate",
+        "text": bmrRate.toString(),
+        "bgColor": const Color(0xFFE8F7FF),
+        "titleColor": Colors.blue,
+        "borderColor": const Color(0xFFB3E5FC),
+      },
+      {
+        "icon": Icons.local_fire_department_outlined,
+        "title": "Maintenance Calories",
+        "text": maintainanceCalorie.toString(),
+        "bgColor": const Color(0xFFE8F5E9),
+        "titleColor": Colors.green,
+        "borderColor": const Color(0xFFC8E6C9),
+      },
+      {
+        "icon": Icons.track_changes,
+        "title": "Target Calories",
+        "text": targetCalories.toString(),
+        "bgColor": const Color(0xFFFFF3E0),
+        "titleColor": Colors.orange,
+        "borderColor": const Color(0xFFFFE0B2),
+      },
+    ];
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ListTile(
+          contentPadding: EdgeInsets.zero,
+          horizontalTitleGap: 8,
+          minLeadingWidth: 0,
+          leading: Icon(Icons.local_fire_department, color: Colors.orange),
+          title: Text(
+            "Your Results",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+          ),
+        ),
+        SizedBox(height: 30),
+        ...resultCards.map(
+          (e) => Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              decoration: BoxDecoration(
+                color: e["bgColor"],
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: e["borderColor"], width: 1),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Icon(e["icon"], color: e["titleColor"]),
+                      SizedBox(width: 6),
+                      Text(
+                        e["title"],
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: e["titleColor"],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(e["text"], style: TextStyle(fontSize: 36, height: 1.1)),
+                  Text(
+                    "calories/day",
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey[600],
+                      height: 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget understandingResultWidget() {
+    late List<Map<String, dynamic>> informativeData = [
+      {
+        'title': "BMR (Basal Metabolic Rate)",
+        'data':
+            "The number of calories your body burns at rest to maintain vital functions like breathing, circulation, and cell production.",
+      },
+      {
+        'title': "Maintenance Calories (TDEE)",
+        'data':
+            "Your Total Daily Energy Expenditure - the calories you burn per day including your activity level. Eating this amount maintains your current weight.",
+      },
+      {
+        'title': "Target Calories",
+        'data':
+            "Same as your TDEE - eat this amount to maintain your current weight.",
+      },
+    ];
+    late Map<String, String> disclaimer = {
+      "label": "Pro Tip: ",
+      "content":
+          "These calculations are estimates. Monitor your progress and adjust your intake based on real results. Combine with regular exercise and a balanced diet for best results. Consider consulting a nutritionist for personalized advice.",
+    };
+    return Column(
+      children: [
+        Text(
+          "Understanding Your Results",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          textAlign: TextAlign.start,
+        ),
+        SizedBox(height: 30),
+        ...informativeData.map(
+          (e) => Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              decoration: BoxDecoration(
+                color: Colors.grey.withAlpha(30),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          e['title'],
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          e['data'],
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[700],
+                            height: 1.5,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 20),
+        Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(width: 1, color: Colors.grey[400]!),
+          ),
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: disclaimer['label'],
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                TextSpan(
+                  text: disclaimer["content"],
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey[900],
+                    height: 1.5,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget macronutritionWidget() {
+    late List<Map<String, dynamic>> macronutrientDistribution = [
+      {
+        "title": "Proteins",
+        "data":
+            "${(targetCalories * 0.32 / 4).round()}g (${((targetCalories * 0.32 / 4).round() * 4)} cal)",
+        "color": Colors.blue,
+      },
+      {
+        "title": "Carbohydrates",
+        "data":
+            "${(targetCalories * 0.38 / 4).round()}g (${((targetCalories * 0.38 / 4).round() * 4)} cal)",
+        "color": Colors.green,
+      },
+      {
+        "title": "Fats",
+        "data":
+            "${(targetCalories * 0.3 / 9).round()}g (${((targetCalories * 0.3 / 9).round() * 9)} cal)",
+        "color": Colors.orange,
+      },
+    ];
+    return Column(
+      children: [
+        Text(
+          "Macronutrient Breakdown",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          textAlign: TextAlign.start,
+        ),
+        SizedBox(height: 30),
+        Text(
+          "Recommended macronutrient distribution for your $targetCalories calorie target:",
+          style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+          textAlign: TextAlign.start,
+        ),
+        SizedBox(height: 30),
+        ...macronutrientDistribution.map(
+          (e) => Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      e["title"],
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    Text(
+                      e["data"],
+                      style: TextStyle(fontSize: 14, color: Colors.grey[700]),
+                    ),
+                  ],
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: LinearProgressIndicator(
+                    value:
+                        double.parse(e["data"].split("(")[1].split(" ")[0]) /
+                        targetCalories,
+                    minHeight: 14,
+                    backgroundColor: Colors.grey.shade300,
+                    valueColor: AlwaysStoppedAnimation(e["color"]),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ],
     );
