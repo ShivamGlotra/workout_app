0e46560a79cb11a612426e357d6997b965e64dc4
Shivam <shivam.glotra.43@gmail.com>
2026-03-23 20:35:39 -0400

    info widget changed

--- lib/widgets/information_widget.dart
+++ lib/widgets/information_widget.dart
@@ -6,57 +6,71 @@ class InformationWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
-      child: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              padding: EdgeInsets.all(6),
-              decoration: BoxDecoration(
-                color: Colors.black12,
-                shape: BoxShape.circle,
-              ),
-              child: Icon(Icons.info, color: Colors.black87),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              top: BorderSide(color: Colors.black.withAlpha(200), width: 6),
             ),
-            SizedBox(height: 10),
-            Text(
-              'How Selection Works',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 12),
-            Text(
-              "Select the muscle groups you want to target in today's session. Your workout will be automatically customized based on your choices to ensure optimal training intensity.",
-              style: TextStyle(letterSpacing: 1, height: 1.3),
-            ),
-            SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton.icon(
-                onPressed: () => Navigator.pop(context),
-                style: ButtonStyle(
-                  elevation: WidgetStateProperty.all(2),
-                  shadowColor: WidgetStateProperty.all(Colors.black87),
-                  backgroundColor: WidgetStateProperty.all(
-                    const Color.fromARGB(221, 43, 43, 43),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(6),
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    shape: BoxShape.circle,
                   ),
-                  foregroundColor: WidgetStateProperty.all(Colors.white),
+                  child: Icon(Icons.info, color: Colors.black87),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'How Selection Works',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 12),
+                Text(
+                  "Select the muscle groups you want to target in today's session. Your workout will be automatically customized based on your choices to ensure optimal training intensity.",
+                  style: TextStyle(letterSpacing: 1, height: 1.7),
+                ),
+                SizedBox(height: 50),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton.icon(
+                    onPressed: () => Navigator.pop(context),
+                    style: ButtonStyle(
+                      elevation: WidgetStateProperty.all(2),
+                      shadowColor: WidgetStateProperty.all(Colors.black87),
+                      backgroundColor: WidgetStateProperty.all(
+                        const Color.fromARGB(221, 43, 43, 43),
+                      ),
+                      foregroundColor: WidgetStateProperty.all(Colors.white),
 
-                  shape: WidgetStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                      shape: WidgetStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    label: Text(
+                      'Got it, thanks!',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
-                label: Text(
-                  'Got it, thanks!',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
